24df493be2d63c5bc0decf373c7094c3d83fb42f
cleilson pereira <marcialwushu@hotmail.com>
2021-03-20 18:38:31 -0300

    :fire: Base arguments Domain  Layer

---
+++ API.Boilerplate.Domain/Arguments/Base/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Domain.Arguments.Base
+{
+    public class Response
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ API.Boilerplate.Domain/Arguments/Usuario/AdicionarUsuarioResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Domain.Arguments.Usuario
+{
+    public class AdicionarUsuarioResponse
+    {
+    }
+}
