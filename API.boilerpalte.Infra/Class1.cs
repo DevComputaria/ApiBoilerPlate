63ea845f233071e36519fcf27160747bfd3e0a32
cleilson pereira <marcialwushu@hotmail.com>
2021-03-21 19:24:03 -0300

    :fire: Add repositorie Usuario

--- API.boilerpalte.Infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace API.boilerpalte.Infra
-{
-    public class Class1
-    {
-    }
-}
---
+++ API.boilerpalte.Infra/Persistence/Repositories/RepositoryUsuario.cs
@@ -0,0 +1,39 @@
+﻿using API.boilerpalte.Infra.Persistence.EF;
+using API.Boilerplate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace API.boilerpalte.Infra.Persistence.Repositories
+{
+    public class RepositoryUsuario
+    {
+        private readonly ApiBoilerplateContext _context;
+
+        public RepositoryUsuario(ApiBoilerplateContext context)
+        {
+            context = _context;
+        }
+
+        public bool Existe(string email)
+        {
+            return _context.Usuarios.Any(x => x.Email.Endereco == email);
+        }
+
+        public Usuario Obter(Guid id)
+        {
+            return _context.Usuarios.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Usuario Obter(string email, string senha)
+        {
+            return _context.Usuarios.FirstOrDefault(x => x.Email.Endereco == email && x.Senha == senha);
+        }
+
+        public void Salvar(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+        }
+    }
+}
