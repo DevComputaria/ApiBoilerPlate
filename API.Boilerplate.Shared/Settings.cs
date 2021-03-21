c2c26b4e27752a05482b09403bb60f8b73fd26aa
cleilson pereira <marcialwushu@hotmail.com>
2021-03-21 19:27:52 -0300

    :fire: Add Settings connectionDB

---
+++ API.Boilerplate.Shared/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Shared
+{
+    public class Settings
+    {
+        private static string connectionString = "Data Source=.;Initial Catalog=YouLearnCurso; persist security info=True;Integrated Security=SSPI;";
+
+        public static string ConnectionString
+        {
+            get { return connectionString; }
+            set { connectionString = value; }
+        }
+    }
+}
