7aa6d7c6755fa8c814fdc66c228e2e281e355ef5
cleilson pereira <marcialwushu@hotmail.com>
2021-03-21 19:26:22 -0300

    :fire: Add IRepositoryUsuario and Service [class and interface]

---
+++ API.Boilerplate.Domain/Interfaces/Repositories/IRepositoryUsuario.cs
@@ -0,0 +1,18 @@
+﻿using API.Boilerplate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryUsuario
+    {
+        Usuario Obter(Guid id);
+
+        Usuario Obter(string email, string senha);
+
+        void Salvar(Usuario usuario);
+
+        bool Existe(string email);
+    }
+}
---
+++ API.Boilerplate.Domain/Interfaces/Services/Base/IServiceBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Domain.Interfaces.Services.Base
+{
+    public interface IServiceBase
+    {
+    }
+}
