e0f1f716fa7e7c19653302caa8033122ad6d5695
cleilson pereira <marcialwushu@hotmail.com>
2021-03-21 19:27:26 -0300

    :fire: Add ValueObjects

---
+++ API.Boilerplate.Domain/ValueObjects/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Domain.ValueObjects
+{
+    public class Email
+    {
+        private string email;
+
+        public Email(string email)
+        {
+            this.email = email;
+        }
+
+        public string Endereco { get; private set; }
+    }
+}
