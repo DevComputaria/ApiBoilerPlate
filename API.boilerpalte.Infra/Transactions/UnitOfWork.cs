5f43978bb326e7c9e896e0e418da6e80a29fb53f
cleilson pereira <marcialwushu@hotmail.com>
2021-03-21 19:22:34 -0300

    :fire: Add UnitOfWork class and interface

---
+++ API.boilerpalte.Infra/Transactions/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using API.boilerpalte.Infra.Persistence.EF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.boilerpalte.Infra.Transactions
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApiBoilerplateContext _context;
+
+        public UnitOfWork(ApiBoilerplateContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
