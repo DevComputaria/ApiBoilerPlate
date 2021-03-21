e129e8b777e618fe532c97fdda78ece385ce4c30
cleilson pereira <marcialwushu@hotmail.com>
2021-03-21 19:24:56 -0300

    :fire: Add Entity usuario

---
+++ API.Boilerplate.Domain/Entities/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using API.Boilerplate.Domain.Entities.Base;
+using API.Boilerplate.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Boilerplate.Domain.Entities
+{
+    public class Usuario : EntityBase
+    {
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(Nome nome, Email email, string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+
+        public Nome Nome { get; private set; }
+
+        public Email Email { get; private set; }
+
+        public string Senha { get; private set; }
+    }
+}
